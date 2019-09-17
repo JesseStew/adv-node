6bdf9fb50cf60bdf84f2174b0bd8ee419038aac1
Jesse Stewart <stewed.jesse@gmail.com>
2019-09-17 09:58:55 -0500

    http module

---
+++ web/http.js
@@ -0,0 +1,15 @@
+const server = require('http').createServer()
+
+server.on('request', (req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html'})
+  // res.end('Hello world\n') // HTTP 1.1: terminates a response with res.end()
+  // ^^^ This is not optional ^^^ if you don't res.end() it will timeout after 2 min
+  res.write('Hello world\n') // Does not terminate response
+
+  setTimeout(() => {
+    res.write('Yet another hello world\n')
+  }, 1000)
+})
+
+server.timeout = 10000
+server.listen(8000)
