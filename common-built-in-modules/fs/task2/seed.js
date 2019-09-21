4eaa471704ac19c77d16484d87a7c382dcb88d97
Jesse Stewart <stewed.jesse@gmail.com>
2019-09-21 12:34:58 -0500

    Added notes to task2 solution.js, removing files older than 7 days

--- common-built-in-modules/fs/task2/seed.js
+++ common-built-in-modules/fs/task2/seed.js
@@ -2,15 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const dirname = path.join(__dirname, 'files');
 
+// Synchonously create a destination directory
 fs.mkdirSync(dirname);
 const ms1Day = 24*60*60*1000;
 
+// Create 10 sample files
 for (let i=0; i < 10; i++) {
   const filePath = path.join(dirname, `file${i}`);
+  
+  // creates files
   fs.writeFile(filePath, i, (err) => {
     if (err) throw err;
 
+    // Subtract 1 day millisecs multiplied by the file index
+    // divide by 1000, to get seconds for fs.utimes
     const time = (Date.now() - i*ms1Day)/1000;
+
+    // Used to change file's timestamp
+    // fs.utimes(filePath, access_time_in_secs, modified_time_in_secs, err) 
     fs.utimes(filePath, time, time, (err) => {
       if (err) throw err;
     });
--- common-built-in-modules/fs/task2/solution.js
+++ common-built-in-modules/fs/task2/solution.js
@@ -5,12 +5,17 @@ const dirname = path.join(__dirname, 'files');
 const files = fs.readdirSync(dirname);
 const ms1Day = 24*60*60*1000;
 
+// Loop over all files in directory
 files.forEach(file => {
   const filePath = path.join(dirname, file);
+
+  // Read stats metadata for each file
   fs.stat(filePath, (err, stats) => {
     if (err) throw err;
 
+    // stats.mtime.getTime()
     if ((Date.now() - stats.mtime.getTime() > 7*ms1Day)) {
+      // fs.unlink() deletes file
       fs.unlink(filePath, (err) => {
         if (err) throw err;
         console.log(`deleted ${filePath}`);
