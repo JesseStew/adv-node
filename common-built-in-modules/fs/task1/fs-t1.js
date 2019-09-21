39105c74f406340ee817ac17a488d9294d39cd42
Jesse Stewart <stewed.jesse@gmail.com>
2019-09-21 12:14:29 -0500

    Added notes for task1

--- common-built-in-modules/fs/task1/fs-t1.js
+++ common-built-in-modules/fs/task1/fs-t1.js
@@ -2,13 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const dirname = path.join(__dirname, 'files')
 
+// Reads the list of files, creates an array of (file names)
 const files = fs.readdirSync(dirname)
 
 files.forEach(file => {
-  const filePath = path.join(dirname, file)
+  // don't use string concatenation
+  // Always use path.join to make your code platform agnostic
+  const filePath = path.join(dirname, file) // gets full path from names
+  
+  // fs.stat give meta info, just need file size, no contents
   fs.stat(filePath, (err, stats) => {
     if (err) throw err
 
+    // Truncates size of files
     fs.truncate(filePath, stats.size/2, (err) => {
       if (err) throw err
     })
